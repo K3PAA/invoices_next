a5dfccd4b14d04c82289de58a829e836a30a5b09
K3PAA <jkepa00@gmail.com>
2024-07-23 13:26:35 +0200

    Adding missing margin on mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,13 +8,13 @@ const DynamicThemeButton = dynamic(() => import("./ThemeButton"), {
 
 export default function Header() {
   return (
-    <header className="flex justify-between bg-draft md:flex-col md:rounded-r-[1.25rem]">
+    <header className="flex justify-between bg-[#373B53] md:flex-col md:rounded-r-[1.25rem]">
       <Logo className="rounded-r-[1.25rem]" />
 
       <div className="flex items-center gap-8 md:flex-col md:justify-center md:gap-0">
         <DynamicThemeButton />
         <div className="h-full w-[1px] bg-[#494E6E] md:mt-8 md:h-[1px] md:w-full"></div>
-        <Avatar className="md:my-6" />
+        <Avatar className="mr-8 md:my-6 md:mr-0" />
       </div>
     </header>
   );
